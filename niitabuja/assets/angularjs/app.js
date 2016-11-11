fd869e39c49d8247f811cc8a882e38d825abce48
samshal <samueladeshina73@gmail.com>
2016-11-11 14:53:29 +0100

    Automated push

--- niitabuja/assets/angularjs/app.js
+++ niitabuja/assets/angularjs/app.js
@@ -20,3 +20,24 @@ angular.module('site', [
 
 	$locationProvider.html5Mode(true);
 })
+
+.controller("courseController", function($scope, $http){
+	$http.get("http://public.niitabuja.com/course-list.php").then(function(success){
+		$scope.courseList = success.data;
+	}, function(error){
+		console.log("error", error);
+	});
+
+	// $scope.courseList = [
+	// 	{"title":".NET Something Something", "description":"Bla bla bla", "icon":"icon-screen"},
+	// 	{"title":".NET Something Something", "description":"Bla bla bla", "icon":"icon-screen"},
+	// 	{"title":".NET Something Something", "description":"Bla bla bla", "icon":"icon-screen"}
+	// ];
+
+	$scope.clearTime = function(index){
+		if (index % 3 == 0){
+			return true;
+		}
+		return false;
+	}
+})
